c0947d503077de9c5884778c8f83be25a49ef70a
srinivas33 <msrinivasa@edyst.com>
2019-09-09 21:54:33 +0530

    add base and home templates and mock objects

--- app/views.py
+++ app/views.py
@@ -1,6 +1,18 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello World!"
+    user = {'username': 'srinivas'}
+    posts = [
+        {
+            'author': {'username': 'Nandu'},
+            'body': 'This post is written by Nandu'
+        },
+        {
+            'author': {'username': 'Srinivas'},
+            'body': 'This post is writeen by Srinivas'
+        }
+    ]
+    return render_template('index.html', user=user, posts=posts)
