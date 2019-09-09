15c2cf9d5a2f432114992a96718b90fde261faa4
srinivas33 <msrinivasa@edyst.com>
2019-09-09 22:57:17 +0530

    add sample login form

--- app/views.py
+++ app/views.py
@@ -1,5 +1,7 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
+
 from app import app
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -16,3 +18,11 @@ def index():
         }
     ]
     return render_template('index.html', user=user, posts=posts)
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for the user {}, rember me'.format(form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title="Sign In", form=form)
