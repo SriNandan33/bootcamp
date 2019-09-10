a06496416455f855c4daa45c7849d5b00fef8600
srinivas33 <msrinivasa@edyst.com>
2019-09-10 23:43:08 +0530

    add User, Post models and migrations

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,14 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
-from app import views
+# extensions
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+
+from app import views, models
--- config.py
+++ config.py
@@ -1,4 +1,9 @@
 import os
 
+BASE_DIR = os.path.abspath(os.path.dirname(__name__))
+
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'this-is-less-secure'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(BASE_DIR, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
