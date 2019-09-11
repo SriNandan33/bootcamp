4e3ed67e119229062993a6adb201064114be60bb
srinivas33 <msrinivasa@edyst.com>
2019-09-11 23:45:30 +0530

    add signup functionality

--- app/views.py
+++ app/views.py
@@ -1,8 +1,8 @@
 from flask import render_template, flash, redirect, url_for, request
 from flask_login import current_user, login_user, logout_user, login_required
 from werkzeug.urls import url_parse
-from app import app
-from app.forms import LoginForm
+from app import app, db
+from app.forms import LoginForm, RegistrationForm
 from app.models import User
 
 @app.route('/')
@@ -44,3 +44,17 @@ def logout():
     logout_user()
     flash('Logged out successfully!')
     return redirect(url_for('login'))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Successfully registered. Please login to continue')
+        return redirect(url_for('login'))
+    return render_template('register.html', title="Sign Up", form=form)
