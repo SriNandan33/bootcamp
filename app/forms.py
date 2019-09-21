bf7fdd368e882888f9f1d3745fd809956af933ec
srinivas33 <msrinivasa@edyst.com>
2019-09-21 17:33:11 +0530

    add form validation to username field in EditProfieForm

--- app/forms.py
+++ app/forms.py
@@ -22,7 +22,7 @@ class RegistrationForm(FlaskForm):
     def validate_username(self, username):
         user = User.query.filter_by(username=username.data).first()
         if user is not None:
-            raise ValidationError("Username is taken!")
+            raise ValidationError("Username is already taken!")
 
     def validate_email(self, email):
         user = User.query.filter_by(email=email.data).first()
@@ -34,3 +34,13 @@ class EditProfileForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     bio = TextAreaField('Bio', validators=[Length(min=0, max=140)])
     submit = SubmitField('Submit')
+
+    def __init__(self, original_username, *args, **kwargs):
+        super(EditProfileForm, self).__init__(*args, **kwargs)
+        self.original_username = original_username
+
+    def validate_username(self, username):
+        if self.original_username != username.data:
+            user = User.query.filter_by(username=username.data).first()
+            if user is not None:
+                raise ValidationError("Username is already taken!")
--- app/views.py
+++ app/views.py
@@ -84,7 +84,7 @@ def user(username):
 
 @app.route('/edit_profile', methods=['GET', 'POST'])
 def edit_profile():
-    form = EditProfileForm()
+    form = EditProfileForm(current_user.username)
     if form.validate_on_submit():
         current_user.username = form.username.data
         current_user.about_me = form.bio.data
