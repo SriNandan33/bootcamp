45cd4cd60f7d17b7a13ddb56e98eaf75bd7ea5e1
Srinivas <nandusrinivas33.cse@gmail.com>
2019-09-23 23:23:41 +0530

    add post create form, view

--- app/forms.py
+++ app/forms.py
@@ -44,3 +44,11 @@ class EditProfileForm(FlaskForm):
             user = User.query.filter_by(username=username.data).first()
             if user is not None:
                 raise ValidationError("Username is already taken!")
+
+
+class PostForm(FlaskForm):
+    post = TextAreaField('Say Something', validators=[
+        DataRequired(),
+        Length(min=1, max=140)
+    ])
+    submit = SubmitField("Submit")
--- app/views.py
+++ app/views.py
@@ -3,8 +3,9 @@ from flask import render_template, flash, redirect, url_for, request
 from flask_login import current_user, login_user, logout_user, login_required
 from werkzeug.urls import url_parse
 from app import app, db
-from app.forms import LoginForm, RegistrationForm, EditProfileForm
-from app.models import User
+from app.forms import LoginForm, RegistrationForm, EditProfileForm, \
+    PostForm
+from app.models import User, Post
 
 @app.before_request
 def before_request():
@@ -12,10 +13,17 @@ def before_request():
         current_user.last_seen = datetime.utcnow()
         db.session.commit()
 
-@app.route('/')
-@app.route('/index')
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
 @login_required
 def index():
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(body=form.post.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash("Your post is now live!")
+        return redirect(url_for('index'))
     posts = [
         {
             'author': {'username': 'Nandu'},
@@ -26,7 +34,7 @@ def index():
             'body': 'This post is writeen by Srinivas'
         }
     ]
-    return render_template('index.html', posts=posts)
+    return render_template('index.html', title="Feed", posts=posts, form=form)
 
 @app.route('/login', methods=["GET", "POST"])
 def login():
