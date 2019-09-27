95fa5e65261b331735619f059d4f36468d59eb57
Srinivas <nandusrinivas33.cse@gmail.com>
2019-09-27 23:30:48 +0530

    include flask moment to make dates human readable

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_migrate import Migrate
+from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
 from config import Config
 
@@ -14,6 +15,7 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
 mail = Mail(app)
+moment = Moment(app)
 
 
 from app import views, models, errors
