41056d698a80c77225031149f149fed6b99b4668
João Victor Monte R Tavares <jotavemonte@gmail.com>
2019-10-04 10:35:18 -0300

    new user field: facebook_id

--- app/models.py
+++ app/models.py
@@ -24,15 +24,16 @@ class User(UserMixin, db.Model):
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
     posts = db.relationship('Post', backref='author', lazy='dynamic')
     followed = db.relationship(
-        'User', 
+        'User',
         secondary=followers,
         primaryjoin=(followers.c.follower_id == id),
         secondaryjoin=(followers.c.followed_id == id),
         backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
+    facebook_id = db.Column(db.String(30), unique=True, index=True)
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
-    
+
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
 
