f417c821793d96f43a5d9b1bfc39aeea0ae0b96a
Timothy Baney <timbaney1989@gmail.com>
2019-10-05 18:54:03 -0500

    tig

--- static/js/follow_unfollow.js
+++ static/js/follow_unfollow.js
@@ -1,7 +1,9 @@
-function flashMessages(data){
-    html = '';
+const Axios = window.axios;
 
-    for (i = 0; i < data.length; i++) {
+function flashMessages(data){
+    let html = '';
+
+    for (let i = 0; i < data.length; i++) {
         html += `
         <div class="alert alert-${data[i]['type']}">
             <a href="#" class="close" data-dismiss="alert">&times;</a>
@@ -9,37 +11,31 @@ function flashMessages(data){
         </div>`;
     }
     return html;
-};
+}
 
 function ajaxGetWithFlash(target_user, follow) {
-    const Axios = window.axios;
     const endpoint = follow ? `/follow/${target_user}` : `/unfollow/${target_user}`;
-    const flashBox = document.getElementById('flash');
-    const followLink = document.getElementById('follow-link')
-    const linkHTML = follow ? (
-        `<button type="button" class="btn btn-info pt-1 pb-1 unfollow-button" onclick="unFollowUser('${target_user}')">
-            Unfollow
-        </button>`
-    ) : (
-        `<button type="button" class="btn btn-info pt-1 pb-1 follow-button" onclick="followUser('${target_user}')">
-            Follow
-        </button>`
-    );
+    const buttonTxt = follow ? "Unfollow" : "Follow";
 
-    console.log(followLink);
-    console.log(linkHTML);
+    const linkHTML = 
+        `<button data-follow="${!follow}" data-user="${target_user}" class="btn btn-info pt-1 pb-1">
+            ${buttonTxt}
+        </button>`;
 
     Axios.get(endpoint).then((response) => {
-        const messages = response.data.flash_messages;
-        const redirect = response.data.redirect;
+        const messages = response.data['flash_messages'];
+        const flashBox = $('#flash');
+        const followLinks = $('.follow-link');
+        const popup = $('.popup');
 
-        if (redirect) {
-            window.location.href = redirect;
-        }
-
-        followLink.innerHTML = linkHTML;
+        followLinks.each((index, _this) => {
+            _this.innerHTML = linkHTML;
+        });
         if (flashBox) {
-            flashBox.innerHTML = flashMessages(messages);
+            flashBox.html(flashMessages(messages))
+        }
+        if (popup) {
+            popup.remove();
         }
     }).catch((error) => {
         console.error(`Error: ${error}, please contact the administrator`);
@@ -53,3 +49,19 @@ function unFollowUser(target_user) {
 function followUser(target_user) {
     ajaxGetWithFlash(target_user, true)
 }
+
+$(function() {
+    $('body').on('click', '.follow-link .btn', (e) => {
+        e.preventDefault();
+
+        const link = $('.follow-link .btn');
+        const follow = link.data('follow');
+        const target_user = link.data('user');
+
+        if (follow) {
+            followUser(target_user);
+        } else {
+            unFollowUser(target_user);
+        }
+    })
+})
---
+++ static/js/hover.js
@@ -0,0 +1,51 @@
+const Axios = window.axios;
+
+const popoverConfig = (element, data) => {
+    return {
+        trigger: 'manual',
+        html: true,
+        animation: false,
+        container: element,
+        content: data,
+    }
+};
+
+$(function () {
+    let timer = null;
+    let xhr;
+    const userPopup = $('.user_popup');
+
+    userPopup.on('mouseenter', (event) => {
+        console.log('Mouse Enter');
+
+        let element = $(event.currentTarget);
+        const username = element.first().text().trim();
+        xhr = Axios.CancelToken.source();
+
+        timer = setTimeout(() => {
+            Axios.get(`/user/${username}/popup`, {
+                cancelToken: xhr.token
+            }).then((response) => {
+                const config = popoverConfig(element, response.data);
+                element.popover({...config}).popover('show');
+            }).catch(error => console.error(error));
+        }, 1000)
+    });
+
+    userPopup.on('mouseleave', (event) => {
+        console.log('Mouse Leave');
+
+        let element = $(event.currentTarget);
+
+        if (timer) {
+            clearTimeout(timer);
+            timer = null;
+        }
+
+        if (xhr) {
+            xhr.cancel('');
+        }
+
+        element.popover('dispose');
+    });
+});
