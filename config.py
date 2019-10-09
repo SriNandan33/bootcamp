6d430db527325e7537974cdefbbe644311ba10ae
0x6f736f646f <blackd0t@protonmail.com>
2019-10-09 13:46:30 +0300

    Removing unnecessary environment variables from the gmail email service

--- config.py
+++ config.py
@@ -9,10 +9,5 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     POSTS_PER_PAGE = 25
     # mail config
-    MAIL_SERVER = os.environ.get('MAIL_SERVER')
-    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
-    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMIN_EMAIL = os.environ.get('ADMIN_EMAIL')
     SENDGRID_API_KEY = os.environ.get('SENDGRID_API_KEY')
