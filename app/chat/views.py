c7019843bce88adf585c4ba21bbd49dcbbae0016
Srinivas <nandusrinivas33.cse@gmail.com>
2019-10-11 19:01:22 +0530

    add endpoint to send message

--- app/chat/views.py
+++ app/chat/views.py
@@ -1,6 +1,6 @@
 from flask import request, jsonify
 from flask_login import login_required, current_user
-from app import pusher
+from app import pusher, db
 from app.chat import bp as chat_bp
 from app.models import User, Channel, Message
 
@@ -27,5 +27,25 @@ def request_chat():
     }
     # trigger an event to recipient
     pusher.trigger(recipient_chat_channel, "new_chat", data)
+
+    return jsonify(data)
+
+@chat_bp.route('/send_message', methods=["POST"])
+@login_required
+def send_message():
+    data = request.json
+    sender_id = data["sender_id"]
+    recipient_id = data["recipient_id"]
+    message = data["message"]
+    channel = data["channel"]
+
+    new_message = Message(body=message, channel_id=channel)
+    new_message.sender_id = sender_id
+    new_message.recipient_id = recipient_id
+    db.session.add(new_message)
+    db.session.commit()
+
+    # send the message to other user
+    pusher.trigger(channel, 'new_message', data)
 
     return jsonify(data)
